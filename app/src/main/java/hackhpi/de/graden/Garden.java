0526f1789e46639e4a7bbc038a0bca0bab75c5d0
Adrian Bartnik <git@adrianbartnik.de>
2018-05-26 22:10:21 +0200

    Updates

--- app/src/main/java/hackhpi/de/graden/Garden.java
+++ app/src/main/java/hackhpi/de/graden/Garden.java
@@ -1,5 +1,15 @@
 package hackhpi.de.graden;
 
 public class Garden {
-
+    public String type = "Private";
+    public String location = "Berlin";
+    public String soil = "ChocolateBananas";
+    public String size = "Very Big";
+    public String listOfPlants = "Jungle";
+    public String owner;
+    public String numberOfMembers = "Everyone";
+    public String livestock;
+    public int imageID;
+    public double lng, lat;
+    public String name;
 }
--- app/src/main/java/hackhpi/de/graden/MainActivity.java
+++ app/src/main/java/hackhpi/de/graden/MainActivity.java
@@ -4,43 +4,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private FrameLayout content;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    MainActivity mainActivity;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-
-                    startActivity(intent);
-//                    return true;
+                    fragment = getMapFragment();
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    Toast.makeText(getApplicationContext(), "Sorry, not implemented :(", Toast.LENGTH_LONG).show();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_LONG).show();
                     return true;
             }
 
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.content, "blub")
+                    .replace(R.id.content, fragment, "blub")
                     .commit();
 
-            return false;
+            return true;
         }
-
     };
 
     @Override
@@ -48,11 +61,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        content = (FrameLayout) findViewById(R.id.content);
-
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    public Fragment getMapFragment() {
+        SupportMapFragment fragment = SupportMapFragment.newInstance();
+
+        fragment.getMapAsync(this);
+
+        return fragment;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        for (Garden garden : getGardens()) {
+            LatLng latLng = new LatLng(garden.lat, garden.lng);
+            googleMap.addMarker(new MarkerOptions().position(latLng).title(garden.name));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                startActivity(intent);
+
+                return true;
+            }
+        });
+    }
+
+    public List<Garden> getGardens() {
+        List<Garden> gardens = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            Garden garden = new Garden();
+
+            garden.name = "Garden " + i;
+            garden.owner = "Owner " + i;
+            garden.lat = 52.520008 + ((new Random().nextBoolean()) ? new Random().nextFloat() * .3 : -new Random().nextFloat() * .3);
+            garden.lng = 13.404954 + ((new Random().nextBoolean()) ? new Random().nextFloat() * .3 : -new Random().nextFloat() * .3);
+
+            gardens.add(garden);
+        }
+
+        return gardens;
+    }
 }
